1140b4c859f93c690c3fa64d0da4b2437f301ca3
alan <cuigaoshun@gmail.com>
2026-08-31 22:35:54 +0800

    init

--- kitex/handler.go
+++ kitex/handler.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 
-	user "example.com/work-demo/common/kitex_gen/test"
+	"example.com/work-demo/common/kitex_gen/test"
 )
 
 // TestServiceImpl implements the TestService interface defined in the IDL.
 type TestServiceImpl struct{}
 
-var _ user.TestService = (*TestServiceImpl)(nil)
+var _ test.TestService = (*TestServiceImpl)(nil)
 
 // TestFields implements the TestService interface.
-func (s *TestServiceImpl) TestFields(ctx context.Context, req *user.TestFieldsRequest) (resp *user.TestFieldsResponse, err error) {
-	return &user.TestFieldsResponse{
+func (s *TestServiceImpl) TestFields(ctx context.Context, req *test.TestFieldsRequest) (resp *test.TestFieldsResponse, err error) {
+	return &test.TestFieldsResponse{
 		Int32Value:      req.GetInt32Value(),
 		Uint32Value:     req.GetUint32Value(),
 		Int64Value:      req.GetInt64Value(),
