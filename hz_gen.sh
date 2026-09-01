174dc5a773d4ab2face84053704ba3964fa0241a
alan <cuigaoshun@gmail.com>
2026-09-01 23:04:50 +0800

    init

--- hz_gen.sh
+++ hz_gen.sh
@@ -8,7 +8,3 @@ MODULE=${MODULE:-example.com/work-demo}
 # Keep generated routes in sync with each HTTP service IDL.
 "$HZ" update --idl api/test/test_api.proto --module "$MODULE" -I . -I api
 "$HZ" update --idl api/work/work.proto --module "$MODULE" -I . -I api
-
-# hz may leave stale generated packages or placeholder handlers behind.
-rm -f internal/handler/test/test_service.go
-rm -rf internal/router/api
